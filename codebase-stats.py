132e18bd032e97c893748b7142bdb1781a48e5d7
Jan Markowski <jan@markowski.ca>
2017-06-13 20:53:57 -0400

    Initial hack of a code report to display file size distributions

---
+++ codebase-stats.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import matplotlib.pyplot as plt
+import glob
+
+
+def generate_dist(files_dct):
+
+    lines = list(files_dct.values())
+
+    n, bins, patches = plt.hist(lines, bins=50,
+                                facecolor='green', edgecolor='black')
+
+    plt.xlabel('Length')
+    plt.ylabel('Files')
+    plt.title(r'File Length Distribution')
+
+    plt.axis([min(lines), max(lines), 0, max(n)])
+    plt.xticks(range(0, max(lines), 200))
+    plt.grid(True)
+
+    plt.show()
+
+
+def collect_files():
+    files_dct = {}
+    files_lst = glob.glob('*.[hc]')
+
+    for f in files_lst:
+        with open(f, 'r') as h:
+            data = h.readlines()
+        files_dct[f] = len(data)
+
+    return files_dct
+
+
+def main():
+    files_dct = collect_files()
+
+    generate_dist(files_dct)
+
+
+if __name__ == '__main__':
+    main()
